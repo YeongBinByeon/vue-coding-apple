382fde3b9d4b8509f490a289cb4cf45dd4f9e917
yeongbin <yeongbin6483@gmail.com>
2025-12-29 11:14:40 +0900

    watcher로 데이터 감시하는 법

--- vuedongsan/src/Modal.vue
+++ vuedongsan/src/Modal.vue
@@ -19,6 +19,17 @@ export default {
   data() {
     return { month: 3 };
   },
+  watch: {
+    month(a, b) {
+      // a: 변경후 데이터, b:변경전 데이터
+      if (isNaN(a)) {
+        alert("숫자만 입력해 주세요!");
+        this.month = 1;
+      } else if (a >= 13) {
+        alert("13이상 입력하지 마셈");
+      }
+    },
+  },
   props: { 원룸들: Array, 누른거: Number, 모달창열렸니: Boolean }, // 데이터이름 : 자료형이름
   methods: {
     send() {
