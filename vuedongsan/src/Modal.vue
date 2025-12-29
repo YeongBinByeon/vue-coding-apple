25cf83d139b96fb04436161801b597e7187f7245
yeongbin <yeongbin6483@gmail.com>
2025-12-29 10:59:01 +0900

    사용자의 input을 받는 법 (v-model)

--- vuedongsan/src/Modal.vue
+++ vuedongsan/src/Modal.vue
@@ -4,8 +4,10 @@
       <img :src="원룸들[누른거].image" style="width: 100%" />
       <h4>{{ 원룸들[누른거].title }}</h4>
       <p>{{ 원룸들[누른거].content }}</p>
-      <p>{{ 원룸들[누른거].price }} 원</p>
-      <Discount />
+      <!-- <input @input="month = $event.target.value" /> -->
+      <!-- 아래와 같은 코드 -->
+      <input v-model.number="month" />
+      <p>{{ month }} 개월 선택함 : {{ 원룸들[누른거].price * month }} 원</p>
       <button @click="send">닫기</button>
     </div>
   </div>
@@ -14,6 +16,9 @@
 <script>
 export default {
   name: "Modal",
+  data() {
+    return { month: 3 };
+  },
   props: { 원룸들: Array, 누른거: Number, 모달창열렸니: Boolean }, // 데이터이름 : 자료형이름
   methods: {
     send() {
