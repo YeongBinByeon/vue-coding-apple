21f9290cc706bf4e302515571dd4c909738e8dec
yeongbin <yeongbin6483@gmail.com>
2026-01-01 22:25:02 +0900

    Part2. 블로그 글목록

---
+++ blog/src/components/List.vue
@@ -0,0 +1,17 @@
+<template>
+  <div>
+    <h5>{{ item.title }}</h5>
+    <p>{{ item.content }}</p>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "List",
+  props:{item: Object, index: Number}
+}
+</script>
+
+<style>
+
+</style>
