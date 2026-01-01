192d9c93ab722384be11615d85d8d2297a9cca71
yeongbin <yeongbin6483@gmail.com>
2026-01-01 22:50:52 +0900

    Part2. vue-rourter 설치와 기본 라우팅

--- blog/src/router/index.js
+++ blog/src/router/index.js
@@ -1,8 +1,26 @@
+import Detail from '@/components/Detail.vue'
+import Home from '@/components/Home.vue'
+import List from '@/components/List.vue'
 import { createRouter, createWebHistory } from 'vue-router'
+
+const routes = [
+  {
+    path: '/list',
+    component: List,
+  },
+  {
+    path: '/',
+    component: Home,
+  },
+  {
+    path: '/detail',
+    component: Detail,
+  },
+]
 
 const router = createRouter({
   history: createWebHistory(import.meta.env.BASE_URL),
-  routes: [],
+  routes: routes,
 })
 
 export default router
