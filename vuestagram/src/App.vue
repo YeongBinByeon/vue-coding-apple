831b41d78d783049b81fe2a0b9433bb2c321a9c4
yeongbin <yeongbin6483@gmail.com>
2026-01-07 23:42:11 +0900

    part3. ajax 요청과 더보기 버튼

--- vuestagram/src/App.vue
+++ vuestagram/src/App.vue
@@ -10,6 +10,7 @@
   </div>
 
   <Container :게시물="게시물" />
+  <button @click="more">더보기</button>
 
   <div class="footer">
     <ul class="footer-button-plus">
@@ -22,14 +23,26 @@
 <script>
 import Container from "./components/Container.vue";
 import postdata from "./assets/postdata.js";
+import axios from "axios";
 
+axios.get();
 export default {
   name: "App",
   data() {
-    return { 게시물: postdata };
+    return { 게시물: postdata, 더보기: 0 };
   },
   components: {
     Container: Container,
+  },
+  methods: {
+    more() {
+      axios
+        .get(`https://codingapple1.github.io/vue/more${this.더보기}.json`)
+        .then((결과) => {
+          this.게시물.push(결과.data);
+          this.더보기++;
+        });
+    },
   },
 };
 </script>
