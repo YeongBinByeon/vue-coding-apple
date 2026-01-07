b9e4a2ce53f75d36688bba2c488c05d0aa10aeee
yeongbin <yeongbin6483@gmail.com>
2026-01-07 23:07:37 +0900

    part3. 프로젝트 생성, 레이아웃 만들기

---
+++ vuestagram/src/components/Container.vue
@@ -0,0 +1,20 @@
+<template>
+  <div>
+    <Post v-for="(a, i) in 게시물" :게시물="게시물[i]" :key="i"></Post>
+  </div>
+</template>
+
+<script>
+import Post from "./Post.vue";
+
+export default {
+  components: {
+    Post: Post,
+  },
+  props: {
+    게시물: Array,
+  },
+};
+</script>
+
+<style></style>
